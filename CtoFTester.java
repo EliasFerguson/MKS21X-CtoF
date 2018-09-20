919b19f5d849b0f18629f1a89d531a104d24f662
Elias Ferguson <eferguson00@stuy.edu>
2018-09-19 21:56:29 -0400

    fixed broken method, all works

--- CtoFTester.java
+++ CtoFTester.java
@@ -4,7 +4,7 @@ public class CtoFTester {
     System.out.println(fahrenheitToCelcius(100));
   }
   static double celciusToFahrenheit(double dCelc) {
-    return dCelc * (9.0 / 5);
+    return dCelc * (9.0 / 5) + 32;
   }
   static double fahrenheitToCelcius(double dFahr) {
     return (dFahr - 32) * (5.0 / 9);
